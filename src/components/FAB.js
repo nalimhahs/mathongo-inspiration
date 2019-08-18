4e5b9a7268d82328220dcb7e40ac64b5ac3d4fe3
Milan Shah <milanshah1@gmail.com>
2019-08-18 11:08:55 +0530

    Added mail modal

--- src/components/FAB.js
+++ src/components/FAB.js
@@ -43,39 +43,125 @@ const Message = styled.div`
   padding: 1rem;
   font-size: 15px;
   padding-left: 1.15rem;
-  display: ${ props => (props.hidden ? "none" : "flex")};
+  display: ${props => (props.hidden ? "none" : "flex")};
   align-items: center;
   font-family: Nunito;
   background: white;
-  
 `;
+
 const Img = styled.img`
   height: 55%;
 `;
 
+const H1 = styled.h1`
+  font-size: 2rem;
+  font-weight: 800;
+  font-family: Nunito;
+  margin-top: 10px;
+`;
+
 export default class FAB extends Component {
-
   state = {
-    hidden: false
-  }
+    hidden: false,
+    modalEmail: "",
+    isModalEmailValid: true,
+    isModalActive: false
+  };
 
-  handleModal = () => {
+  handleModalShow = () => {
+    this.setState({ isModalActive: true });
+  };
 
+  handleModalClose = () => {
+    this.setState({ isModalActive: false });
   };
 
   handleMessageHide = () => {
-    this.setState({hidden: true})
-  }
+    this.setState({ hidden: true });
+  };
+
+  handleModalEmailSubmit = () => {
+    var email = this.state.modalEmail;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(email).toLowerCase())) {
+      this.setState({ isModalEmailValid: false });
+    } else {
+      this.setState({ isModalEmailValid: true });
+      // Handle form submit here
+      this.handleModalClose();
+    }
+  };
+
+  handleEnterSubmit = event => {
+    var code = event.keyCode || event.which;
+    if (code === 13) {
+      this.handleModalEmailSubmit();
+    }
+  };
 
   render() {
     return (
       <div>
-        <FABWrapper onClick={this.handleModal} onMouseLeave={this.handleMessageHide}>
+        <FABWrapper
+          onClick={this.handleModalShow}
+          onMouseLeave={this.handleMessageHide}
+        >
           <Img src={mail} />
         </FABWrapper>
-        <Message hidden={this.state.hidden} onMouseLeave={this.handleMessageHide}>
+        <Message
+          hidden={this.state.hidden}
+          onMouseLeave={this.handleMessageHide}
+        >
           Drop in your mail to get the best delivered to your inbox daily.
         </Message>
+        <div className={this.state.isModalActive ? "modal is-active" : "modal"}>
+          <div className="modal-background" onClick={this.handleModalClose} />
+          <div className="modal-content">
+            <div className="box">
+              <div className="container">
+                <H1>Get inspiration. Delivered!</H1>
+                <hr />
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input
+                      className={
+                        this.state.isModalEmailValid
+                          ? "input"
+                          : "input is-danger"
+                      }
+                      type="email"
+                      placeholder="Email input"
+                      onChange={e => {
+                        this.setState({ modalEmail: e.target.value });
+                      }}
+                      value={this.state.modalEmail}
+                      onKeyDown={this.handleEnterSubmit}
+                    />
+                  </div>
+                  {this.state.isModalEmailValid ? (
+                    ""
+                  ) : (
+                    <p className="help is-danger">This email is invalid</p>
+                  )}
+                </div>
+                <button
+                  className="button is-link"
+                  aria-label="submit"
+                  onClick={this.handleModalEmailSubmit}
+                  style={{ marginBottom: "20px", marginTop: "10px" }}
+                >
+                  Sign Me Up!
+                </button>
+              </div>
+            </div>
+          </div>
+          <button
+            className="modal-close is-large"
+            aria-label="close"
+            onClick={this.handleModalClose}
+          />
+        </div>
       </div>
     );
   }
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from '../components/Navbar'
-import FAB from '../components/FAB'
-import MainWrapper from '../components/MainWrapper'
+import Navbar from "../components/Navbar";
+import FAB from "../components/FAB";
+import MainWrapper from "../components/MainWrapper";
 
 export default function MainView() {
   return (
     <>
       <Navbar />
-      <MainWrapper/>
+      <MainWrapper />
       <FAB />
     </>
-  )
+  );
 }
